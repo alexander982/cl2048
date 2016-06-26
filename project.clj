1bef0c7a5dac646773f3d0d6271aec35cf277942
Alexander Kozlov <asmailiks@gmail.com>
2016-06-26 12:09:26 +0300

    Update clojure dependency to 1.8.0

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject cl2048 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+(defproject cl2048 "0.1.0"
+  :description "Clone of 2048 game"
+  :url "https://github.com/alexander982/cl2048"
+
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot cl2048.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
